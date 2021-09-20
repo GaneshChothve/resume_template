3c7c51e66f4b9da6d9c950f210363e5004b950e0
Ganesh Chothve <84216414+GaneshChothve@users.noreply.github.com>
2021-09-20 12:31:09 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// const a = ['img/ganesh_chothve.png','img/Koala.jpg']
+
+// const img = document.getElementById('img-con')
+// let i=0
+// function imgchanger() {
+//     img.style.background= `url('${a[Math.floor(Math.random() * 2)]}') no-repeat `
+//     img.style.backgroundSize = 'cover'
+//     console.log(a[Math.floor(Math.random() * 2)])
+// }
+
+// id=setInterval(imgchanger, 1000)
+
+
